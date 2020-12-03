081a5ab61a40fda4cb5c7d0d0830623993b5aff2
Jacsksonchoii <cccctae@gmail.com>
2020-12-03 23:47:48 +0900

    practice07

---
+++ src/com/javaex/ex03/Cat.java
@@ -0,0 +1,9 @@
+package com.javaex.ex03;
+
+public class Cat implements Soundable {
+
+	public void sound() {
+		System.out.println("야옹");
+	}
+
+}
---
+++ src/com/javaex/ex03/Dog.java
@@ -0,0 +1,9 @@
+package com.javaex.ex03;
+
+public class Dog implements Soundable {
+
+	public void sound() {
+		System.out.println("멍멍");
+	}
+
+}
